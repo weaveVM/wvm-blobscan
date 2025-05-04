29379c1c1afa47c4131ee4a2b878bb40faa9c1c9
charmful0x <charmful0x@gmail.com>
2025-05-04 14:54:47 +0200

    feat: switch to blobscan REST API

--- src/utils/blobscan.rs
+++ src/utils/blobscan.rs
@@ -1,21 +1,18 @@
 use {
-    crate::utils::env_var::get_env_var,
-    crate::utils::{
-        planetscale::{ps_archive_block, ps_get_all_versioned_hashes_paginated},
-        types::BlobInfo,
-        wvm::send_wvm_calldata,
-    },
+    crate::utils::{env_var::get_env_var, planetscale::{ps_archive_block, ps_get_all_versioned_hashes_paginated}, types::BlobInfo, wvm::send_wvm_calldata},
     eyre::{eyre, Error, Result},
     foundry_blob_explorers::{BlockResponse, Client},
-    reqwest, serde_json,
+    reqwest, serde_json::{self, Value},
     std::io::{Read, Write},
 };
 
 pub async fn get_block_by_id(block_id: u32) -> Result<BlockResponse, Error> {
+    println!("GETTING BLOCK: {:?}", block_id);
     let block_id = block_id.to_string();
     let client = Client::mainnet();
 
     let block = client.block(block_id.parse().unwrap()).await;
+    println!("BLOCK: {:?}", block);
 
     match block {
         Ok(block) => Ok(block),
@@ -26,23 +23,52 @@ pub async fn get_block_by_id(block_id: u32) -> Result<BlockResponse, Error> {
     }
 }
 
-pub fn get_blobs_of_block(block: BlockResponse) -> Result<Vec<BlobInfo>> {
+pub async fn get_blobs_versioned_hashes_of_block(block_id: u32) -> Result<Vec<String>, eyre::Error> {
+    let url = format!("https://api.blobscan.com/blocks/{}?type=canonical", block_id);
+    let req: Value = reqwest::Client::new().get(url).send().await.unwrap().json().await?;
+    let versioned_hashes: Vec<String> = req
+    .pointer("/transactions")
+    .and_then(|txs| txs.as_array())
+    .map(|txs| {
+        txs.iter()
+            .filter_map(|tx| tx.pointer("/blobs"))
+            .filter_map(|blobs| blobs.as_array())
+            .flat_map(|blobs| {
+                blobs.iter()
+                    .filter_map(|blob| blob.pointer("/versionedHash"))
+                    .filter_map(|hash| hash.as_str())
+                    .map(String::from)
+                    .collect::<Vec<String>>()
+            })
+            .collect()
+    })
+    .unwrap_or_default();
+    Ok(versioned_hashes)
+
+}
+
+async fn get_blob_data(versioned_hash: &str) -> Result<String, eyre::Error> {
+    let url = format!("https://api.blobscan.com/blobs/{}/data", versioned_hash);
+    let res = reqwest::Client::new().get(url).send().await?.text().await.unwrap_or_default();
+    let data = res.to_string();
+    // println!("\n\n\n\n BLOB DATA:\n {}\n\n\n", data);
+    Ok(data)
+
+}
+
+pub async fn get_blobs_of_block(block_id: u32) -> Result<Vec<BlobInfo>> {
+    let versioned_hashes = get_blobs_versioned_hashes_of_block(block_id).await.unwrap_or_default();
     let mut res: Vec<BlobInfo> = Vec::new();
-    let txs = block.transactions;
-    for _tx in txs {
-        let blobs = _tx.blobs;
+    for hash in versioned_hashes {
+        let blob_data = get_blob_data(&hash).await.unwrap();
 
-        println!("BLOCKS COUNT IN BLOCK #{} is {}", block.number, blobs.len());
+        let blob = BlobInfo {
+            ethereum_block_number: block_id as u64,
+            versioned_hash: hash,
+            data: blob_data
+        };
 
-        for _blob in blobs {
-            let to_blob_info = BlobInfo::from(
-                block.number,
-                _blob.versioned_hash.to_string(),
-                _blob.data.to_string(),
-            );
-            // println!("{:?}", to_blob_info);
-            res.push(to_blob_info);
-        }
+        res.push(blob);
     }
 
     Ok(res)
@@ -54,15 +80,13 @@ pub fn serialize_blobscan_block(block: &BlobInfo) -> Result<Vec<u8>> {
     Ok(compressed_data)
 }
 
-pub async fn insert_block(block: BlockResponse) -> Result<(), Error> {
-    let blobs = get_blobs_of_block(block.clone())?;
+pub async fn insert_block(block_id: u32, blobs: Vec<BlobInfo>) -> Result<(), Error> {
 
     for blob in blobs {
         let wvm_data_input = serialize_blobscan_block(&blob)?;
-        let raw_block_data = serde_json::to_string(&blob)?;
         let wvm_txid = send_wvm_calldata(wvm_data_input).await.unwrap();
-        let res = ps_archive_block(
-            &(block.clone().number as u32),
+        let _res = ps_archive_block(
+            &block_id,
             &wvm_txid,
             &blob.versioned_hash,
             &blob.data,
--- src/utils/constants.rs
+++ src/utils/constants.rs
@@ -3,3 +3,4 @@ pub const ETH_RPC_URL: &str = "https://eth.llamarpc.com";
 pub const WVM_RPC_URL: &str = "https://alphanet.load.network";
 pub const WVM_CHAIN_ID: u32 = 9496;
 pub const WVM_ARCHIVER_ADDRESS: &str = "0x2BC885e26A3947646FAbb96C68cE82f5937038a7";
+pub const PARALLEL_WORKERS: usize = 50; 
